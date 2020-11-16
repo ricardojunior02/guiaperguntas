0012034bdc0d7a18e78117704ec420b780cb53f0
ricardojunior02 <ricardojunior199502@gmail.com>
2020-11-16 19:35:42 -0300

    Model de perguntas

---
+++ src/models/Ask.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const connection = require('../database/database');
+
+const Ask = connection.define('asks', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+});
+
+Ask.sync({ force: false }).then(() => {});
+
+
+module.exports = Ask;
